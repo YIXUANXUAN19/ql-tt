e8a9c55d52f71ff84445ced87454c8f7da68572c
YIXUANXUAN19 <44570514+YIXUANXUAN19@users.noreply.github.com>
2022-05-05 17:54:47 +0800

    Update ksjsb_zjb.py

--- ksjsb_zjb.py
+++ ksjsb_zjb.py
@@ -4,32 +4,18 @@
 cron: 20 10 * * *
 new Env('快手极速版-周周十五万');
 
-@Time   : 2022/4/14 15:50
-@Author : XuanYi
-@File   : kszjb.py
-@desc   :
-每个账号每天都有一次助力机会，但是你一周内只能被同一账号助力一次
+变量：ksjsbAssistance 指定助力码
+export ksjsbAssistance='助力码'
 
-export ksjsbAssistance=''
-变量：ksjsbAssistance 指定助力码,则所有账号只助力这个助力码
-如果没有指定则车头每周助力一次作者，其他号根据排序来互相助力，首先助力排序靠前的号,能保证每个号都能被助力到，实现收益最大化
-
-抓包：二选一
-尽量使用完整cookie，使用部分cookie可能会导致快手返回的信息错误
-推荐完整cookie:  开头是 kpn=NEBULA; kpf=ANDROID_PHONE;，中间有 did=XXX; kuaishou.api_st=XXXXX;
-也可以部分cookie：kuaishou.api_st=XXXXX; did=XXX;   可以只要 kuaishou.api_st=XXX; 但是如果有did的最好把did加上
+ck格式需要包含 did 和 kuaishou.api_st :  did=XXX; kuaishou.api_st=XXXXX;
 
 ck变量格式：三选一
-ksjsbCookie='ck'
-可以放环境变量里，一个变量一个ck
+可以放环境变量里，一个变量一个ck  ksjsbCookie='ck'
 也可以使用 export ksjsbCookie='ck1@ck2@ck3'
 也可以使用 export ksjsbCookie='ck1&ck2&ck3'
 
-可有可无变量：
-自定义 User-Agent：export kszjbUA='XXXXX'
-设置助力一个账号后的等待时间(默认5秒)：export kszjbWait='5'
-
-如果助力顺序出问题，就删除 kszjb.py.json 文件，这个文件是记录谁给谁助力过的
+设置每个账号的间隔时间，设为为0则不等待，默认10秒
+export kszjbWait='0'
 """
 import os
 import sys
